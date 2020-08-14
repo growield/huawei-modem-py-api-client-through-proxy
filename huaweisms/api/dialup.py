8cae130f43871938392292e527b79bb15f314fd3
m-krasikov <growield@gmail.com>
2020-08-14 15:09:51 +0300

    Implemented ability to switch net mode

--- huaweisms/api/dialup.py
+++ huaweisms/api/dialup.py
@@ -45,3 +45,41 @@ def switch_mobile_on(ctx: huaweisms.api.common.ApiCtx, proxy=None):
     }
     url = "{}/dialup/mobile-dataswitch".format(ctx.api_base_url)
     return huaweisms.api.common.post_to_url(url, data, ctx, additional_headers=headers, proxy=proxy)
+
+
+def switch_net_mode(ctx: huaweisms.api.common.ApiCtx, net_mode='lte_umts', proxy=None):
+    xml_template = (
+    '<?xml version="1.0" encoding="UTF-8"?>'
+    '<request>'
+    '<NetworkMode>{mode}</NetworkMode>'
+    '<NetworkBand>3FFFFFFF</NetworkBand>'
+    '<LTEBand>7FFFFFFFFFFFFFFF</LTEBand>'
+    '</request>'
+    )
+    if net_mode == 'lte':
+        data = xml_template.format(mode='03')
+    elif net_mode == 'umts':
+        data = xml_template.format(mode='02')
+    elif net_mode == 'lte_umts':
+        data = xml_template.format(mode='0302')
+    else:
+        data = xml_template.format(mode='0302')
+    headers = {
+        '__RequestVerificationToken': ctx.token,
+    }
+    url = "{}/net/net-mode".format(ctx.api_base_url)
+    return huaweisms.api.common.post_to_url(url, data, ctx, additional_headers=headers, proxy=proxy)
+
+
+def get_net_mode(ctx: huaweisms.api.common.ApiCtx, proxy=None):
+    url = "{}/net/net-mode".format(ctx.api_base_url)
+    result = huaweisms.api.common.get_from_url(url, ctx, proxy=proxy)
+    if result and result.get('type') == 'response':
+        response = result['response']
+        if response and response.get('NetworkMode') == '0302':
+            return 'lte_umts'
+        if response and response.get('NetworkMode') == '03':
+            return 'lte'
+        if response and response.get('NetworkMode') == '02':
+            return 'umts'
+    return 'UNKNOWN_MODE'
